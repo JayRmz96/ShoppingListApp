6531553e43b6618928f7f3a8283804eada816b03
Juan Ramirez <avengedforromance@hotmail.com>
2018-09-26 17:55:06 -0500

    119, something went wrong

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyA0uCuu99QOzcPY-Popj4j3Szcea2RNcsM",
+  authDomain: "shoppinglistionic-7e304.firebaseapp.com",
+  databaseURL: "https://shoppinglistionic-7e304.firebaseio.com",
+  projectId: "shoppinglistionic-7e304",
+  storageBucket: "shoppinglistionic-7e304.appspot.com",
+  messagingSenderId: "352253630472"
+};
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { IonicPage } from "ionic-angular";
+import {ShoppingListService} from "../../services/shopping-list/shopping-list.service";
+import {Observable} from "rxjs";
+import {Item} from "../../models/item/item.model";
 
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  shoppingList$: Observable<Item[]>;
+  constructor(public navCtrl: NavController, private shopping: ShoppingListService) {
+    this.shoppingList$ = this.shopping
+      .getShoppingList()  //Returns a DB List
+      .snapshotChanges() //Return Key and value pairs
+      .map(
+        changes =>{
+              return changes.map( c=> ({
+                      key: c.payload.key, ...c.payload.val()
+              }));
+        });
   }
 
 }
